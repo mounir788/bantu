6c15584ed12490ae4f09ff012dc89aa40d1e0502
Mounir Ahmed <mounerelgazzar@gmail.com>
2025-09-27 07:08:51 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,54 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("contactForm");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    clearErrors();
+
+    let isValid = true;
+
+    // Name validation
+    const name = document.getElementById("name");
+    if (name.value.trim() === "") {
+      showError(name, "Full name is required");
+      isValid = false;
+    }
+
+    // Email validation
+    const email = document.getElementById("email");
+    if (email.value.trim() === "") {
+      showError(email, "Email is required");
+      isValid = false;
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value.trim())) {
+      showError(email, "Enter a valid email address");
+      isValid = false;
+    }
+
+    // Message validation
+    const message = document.getElementById("message");
+    if (message.value.trim() === "") {
+      showError(message, "Message cannot be empty");
+      isValid = false;
+    }
+
+    if (isValid) {
+      alert("Form submitted successfully!");
+      form.reset();
+    }
+  });
+
+  // show error under input
+  function showError(input, message) {
+    const wrapper = input.parentElement;
+    let error = wrapper.nextElementSibling;
+
+    error.textContent = message;
+  }
+
+  // clear all errors
+  function clearErrors() {
+    document
+      .querySelectorAll(".error__message")
+      .forEach((el) => (el.textContent = ""));
+  }
+});
